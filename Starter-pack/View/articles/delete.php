1cc0ba3e6769353ada495f38e7ebb874b2360b41
PIeter <pieter_ophalvens@hotmail.com>
2024-01-19 17:22:46 +0100

    fixing missing quotes on delete and edit page

--- Starter-pack/View/articles/delete.php
+++ Starter-pack/View/articles/delete.php
@@ -3,13 +3,14 @@
 <h2>Confirm Delete:</h2>
 <form method='POST'>
   <label for="title">title:</label>
-  <input disabled='true' class='title' value=<?= $edit["title"] ?> name='title' type='text'> </input>
+  <input disabled='true' class='title' value='<?= $edit["title"] ?>' name='title' type='text'> </input>
   <label for="description">description:</label>
-  <input disabled='true' class='description' value=<?= $edit["description"] ?> name='description' type='text'> </input>
+  <input disabled='true' class='description' value='<?= $edit["description"] ?>' name='description' type='text'>
+  </input>
   <label for="publishdate">publishdate: </label>
-  <input disabled='true' class='publishdate' value=<?= $edit["publishdate"] ?> name='publishdate' type='text'></input>
+  <input disabled='true' class='publishdate' value='<?= $edit["publishdate"] ?>' name='publishdate' type='text'></input>
   <label for="image">image:</label>
-  <input disabled='true' class='image' <?= $edit["image"] ?? '' ?> name='image' type='text'>
+  <input disabled='true' class='image' value='<?= $edit["image"] ?? ' ' ?> ' name='image' type='text'>
   </input>
   <input type="submit" style="color:red" name=delete value='confirm'>
 </form>
--- Starter-pack/View/articles/edit.php
+++ Starter-pack/View/articles/edit.php
@@ -2,11 +2,9 @@
 
 <form method='POST'>
   <label for="title">title:</label>
-  <input class='title' value=<?= $edit["title"] ?> name='title' type='text'> </input>
+  <input class='title' value='<?= $edit["title"] ?>' name='title' type='text'> </input>
   <label for="description">description:</label>
-  <input class='description' value=<?= $edit["description"] ?> name='description' type='text'> </input>
-  <label for="publishdate">publishdate: </label>
-  <input class='publishdate' value=<?= $edit["publish_date"] ?> name='publishdate' type='text'></input>
+  <input class='description' value='<?= $edit['description'] ?>' name='description' type='text'> </input>
   <label for="image">image:</label>
   <input class='image' name='image' type='text' value=<?= $edit["image"] ?? '' ?>>
   </input>
